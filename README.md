ec3d75b204dee934f03b825f37ef557cf3a8b118
ngathan <hanhngabg@gmail.com>
2022-01-04 13:51:58 -0500

    add MLP

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Building DL models to classify images using MNIST dataset
 
+
+# MLP 
+
 The task is to build a 2 layer MLP to classify images.  
 
 Dataset: https://pytorch.org/vision/stable/datasets.html#mnist
@@ -19,4 +22,20 @@ The goal is to train a 2 layer MLP network with the following structure:
 5. At the end, a softmax layer (to generate probabilities over these 10 digits)
 
 Use log-loss also called cross-entropy loss. Also, report accuracy where you predict the digit with the highest probability. Try to get 90%+ accuracy on the test set.
+
+# CNN 
+
+The task is to build a 2-layer CNN to classify images. CNN is known to be much better at image classificastion task.   
+
+CNN architecture:
+        
+1. CNN layer + relu 
+2. CNN layer + relu 
+3. flatten  
+4. Linear layer to an output vector of size 84 (one for each digit) + relu 
+4. Linear layer to an output vector of size 10 (one for each digit)
+5. At the end, a softmax layer (to generate probabilities over these 10 digits)
+
+Use log-loss also called cross-entropy loss. Also, report accuracy where you predict the digit with the highest probability. Try to get 90%+ accuracy on the test set.
+
 
---
+++ src/MLP.py
@@ -0,0 +1,112 @@
+import torch
+from torch import nn as nn      # neural network within torch
+from torchvision import datasets, transforms
+from torch import optim
+import torch.nn.functional as f
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+
+
+class MLP(nn.Module):  # noqa
+
+    def __init__(self):
+        nn.Module.__init__(self)
+        self.network = nn.Sequential(
+            nn.Linear(32, 50),
+            nn.ReLU(),
+            nn.Linear(50, 60),
+            nn.ReLU(),
+            nn.Linear(60,10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        # pdb.set_trace()
+        # x = self.pool(f.relu(self.conv1(x)))
+        # x = self.pool(f.relu(self.conv2(x)))
+        # x = torch.flatten(x, 1)
+        # x = f.relu(self.fc1(x))
+        # x = self.fc2(x)
+        return  self.network(x)
+
+def im_show(img):
+    img = img / 2 + 0.5
+    np_img = img.numpy()
+    plt.imshow(np.transpose(np_img, (1, 2, 0)))
+    plt.show()
+
+
+def train():
+
+    batch_size = 4
+    transform = transforms.Compose(
+                [transforms.ToTensor(),
+                 transforms.Normalize([0.5], [0.5])])  # transform for black/ white images
+    train_data = datasets.MNIST('../data', train=True,
+                                download=True, transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+                                               shuffle=True, num_workers=2)
+
+    # define a MLP:
+    mlp = MLP()
+
+    # define a loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(mlp.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):
+
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+            if i % 2000 == 0:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    # save training model
+    path = './mnist_train.pth'
+    torch.save(net.state_dict(), path)
+    print("Finished Training")
+
+
+def test():
+    path = './mnist_train.pth'
+    net = Net()
+    net.load_state_dict(torch.load(path))
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize([0.5], [0.5])])  # transform for black/ white images
+
+    batch_size = 4
+    test_data = datasets.MNIST('../data', train=False,
+                               download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy on the test set:', 100 * correct / total)
+
+
+if __name__ == "__main__":
+    train()
+    test()
